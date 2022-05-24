47804696f6c9498c116504e16ffee729aeba0ddf
Sheroz Samatov <sherozonline@gmail.com>
2022-05-24 15:49:20 +0500

    Lesson 8, while loops. Not complete yet.

---
+++ lesson-8.js
@@ -0,0 +1,31 @@
+// while loop
+
+// let count = 1;
+
+// while(count < 10) {
+//   console.log('Hello, world!');
+//   count++;
+// }
+
+
+// let count = 1;
+
+// while(count <= 10) {
+//   console.log(count, 'Hello world');
+//   count++;
+// }
+
+
+// let count = 10;
+
+// while(count >= 1) {
+//   console.log(count, 'Hello world');
+//   count--;
+// }
+
+// let count = 1;
+
+// while(count <= 10) {
+//   if(count % 2 !== 0) console.log(count);
+//   count++;
+// }
