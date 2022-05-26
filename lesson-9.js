805865eea62a4e344679720b6b46abbdee7b8757
Sheroz Samatov <sherozonline@gmail.com>
2022-05-26 15:40:11 +0500

    add exercise

--- lesson-9.js
+++ lesson-9.js
@@ -112,3 +112,48 @@
 // for (let key in names) {
 //   console.log(key, names[key]); // выводит на консоль индексы и элементы массива.
 // }
+
+
+function word(s) {
+  switch(s) {
+    case 'one':
+      console.log(1);
+      break;
+      case 'two':
+      console.log(2);
+      break;
+      case 'three':
+      console.log(3);
+      break;
+      case 'four':
+      console.log(4);
+      break;
+      case 'five':
+      console.log(5);
+      break;
+      case 'six':
+      console.log(6);
+      break;
+      case 'seven':
+      console.log(7);
+      break;
+      case 'eight':
+      console.log(8);
+      break;
+      case 'nine':
+      console.log(9);
+      break;
+      case 'zero':
+      console.log(0);
+      break;
+      default:
+      console.log('Not a number from this list')
+      
+  }
+}
+
+word("one") //➞ 1
+
+word("two") //➞ 2
+
+word("nine") //➞ 9
