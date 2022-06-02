11f81212a607c0afde3bd2e2b9107921b246ca7b
Sheroz Samatov <sherozonline@gmail.com>
2022-06-02 12:26:26 +0500

    saving changes

--- lesson-11.js
+++ lesson-11.js
@@ -131,19 +131,4 @@ html
 // console.log(firstName);
 // console.log(lastName);
 
-// ***************** index.js *****************
-// Lesson 13 - Method chaining in JavaScript
-
-// method chaining = calling one method after another
-// in one continuous line of code
-
-// let userName = "sheroz";
-
-// let letter = userName.charAt(0);
-// letter = letter.toUpperCase();
-
-// let letter = userName.charAt(0).toUpperCase().trim(); // is equivalent to the above code
-
-// console.log(letter);
-
 
